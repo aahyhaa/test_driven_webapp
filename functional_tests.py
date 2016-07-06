24681a52eb4cc55587d5037ad35339a8d2d8edaf
houyahui <houyahui@t2cloud.net>
2016-07-06 14:50:06 +0800

    add new list item

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser=webdriver.Chrome()
-        #self.browser.implicitly_wait(5)
+        self.browser.implicitly_wait(5)
+    '''
     def tearDown(self):
 	self.browser.quit()
+    '''
     def test_can_start_a_list_and_retrieve_it_later(self):
 	self.browser.get('http://localhost:8000')
 	self.assertIn('To-Do',self.browser.title)
@@ -15,10 +18,16 @@ class NewVisitorTest(unittest.TestCase):
 	inputbox=self.browser.find_element_by_id('id_new_item')
 	self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
 	inputbox.send_keys('Buy peacock feathers')
-	#inputbox.send_keys(Keys.ENTER)
+	inputbox.send_keys(Keys.ENTER)
 	table=self.browser.find_element_by_id('id_list_table')
 	rows = table.find_elements_by_tag_name('tr')
-	self.assertTrue(any(row.text=='1: Buy peacock feathers' for row in rows),'New to-do item did not appear in table')	
+	self.assertIn('1: Buy peacock feathers',[row.text for row in rows])	
+	inputbox=self.browser.find_element_by_id('id_new_item')
+	inputbox.send_keys('Use peacock feathers to make a fly')
+	inputbox.send_keys(Keys.ENTER)
+	table=self.browser.find_element_by_id('id_list_table')
+	rows = table.find_elements_by_tag_name('tr')
+	self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows]) 
 	self.fail('Finish the test!')
 if __name__=='__main__':
 	unittest.main()
