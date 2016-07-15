f373e4a149676b85ef60ce69289738060a88e300
houyahui <houyahui@t2cloud.net>
2016-07-15 18:17:59 +0800

    new URL ,view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -9,6 +9,16 @@ from lists.models import Item
     def test_bad_math(self):
         self.assertEqual(1+1,3)
 '''
+class ListViewTest(TestCase):
+    def test_home_page_displays_all_list_items(self):
+	Item.objects.create(text='itemey 1')
+	Item.objects.create(text='itemey 2')
+	response=self.client.get('/lists/the-only-list-in-the-world/')
+	self.assertContains(response,'itemey 1')
+	self.assertContains(response,'itemey 2')
+    def test_uses_list_template(self):
+	response=self.client.get('/lists/the-only-list-in-the-world/')
+	self.assertTemplateUsed(response,'list.html')
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found=resolve('/')
@@ -33,7 +43,7 @@ class HomePageTest(TestCase):
 	new_item=Item.objects.first()
 	self.assertEqual(new_item.text,'A new list item')
 	self.assertEqual(response.status_code,302)
-	self.assertEqual(response['location'],'/')
+	self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
     def test_home_page_only_saves_items_when_necessary(self):
 	request=HttpRequest()
 	home_page(request)
@@ -44,14 +54,7 @@ class HomePageTest(TestCase):
 	request.POST['item_text']='A new list item'
 	response=home_page(request)
 	self.assertEqual(response.status_code,302)
-	self.assertEqual(response['location'],'/')
-    def test_home_page_displays_all_list_items(self):
-	Item.objects.create(text='itemey 1')
-	Item.objects.create(text='itemey 2')
-	request=HttpRequest()
-	response=home_page(request)
-	self.assertIn('itemey 1',response.content)
-	self.assertIn('itemey 2',response.content)
+	self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
 	first_item=Item()
