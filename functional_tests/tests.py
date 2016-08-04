6969656954e3f33915fda0363e42578138efc163
houyahui <houyahui@t2cloud.net>
2016-08-04 11:26:58 +0800

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+'''
+The name of the class should be StaticLiveServerTestCase.
+It is renamed just before the 1.7 release.
+I think the book is based on the beta version of Django 1.7. 
+UPDATE  Make sure import it:  from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+'''
 from django.test import LiveServerTestCase
 import time
 import unittest
-class NewVisitorTest(LiveServerTestCase,unittest.TestCase):
+class NewVisitorTest(StaticLiveServerTestCase,unittest.TestCase):
     def test_layout_and_styling(self):
 	self.browser.get(self.live_server_url)
 	self.browser.set_window_size(1024,768)
-	
 	inputbox=self.browser.find_element_by_id('id_new_item')
 	inputbox.send_keys('testing\n')
 	inputbox=self.browser.find_element_by_id('id_new_item')
@@ -17,6 +23,7 @@ class NewVisitorTest(LiveServerTestCase,unittest.TestCase):
         self.browser.implicitly_wait(15)
 	#self.live_server_url='http://localhost:8000/'
     def tearDown(self):
+	self.browser.refresh()
 	self.browser.quit()
     def check_for_row_in_list_table(self,row_text):
 	table=self.browser.find_element_by_id('id_list_table')
@@ -40,10 +47,8 @@ class NewVisitorTest(LiveServerTestCase,unittest.TestCase):
 	inputbox.send_keys(Keys.ENTER)
 	self.check_for_row_in_list_table('1: Buy peacock feathers')
 	self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-	
 	self.browser.quit()
-	self.browser=webdriver.Firefox()
-	
+	self.browser=webdriver.Chrome()
 	self.browser.get(self.live_server_url)
 	page_text=self.browser.find_element_by_tag_name('body').text
 	self.assertNotIn('Buy peacock feathers',page_text)
@@ -51,11 +56,9 @@ class NewVisitorTest(LiveServerTestCase,unittest.TestCase):
 	inputbox=self.browser.find_element_by_id('id_new_item')
 	inputbox.send_keys('Buy milk')
 	inputbox.send_keys(Keys.ENTER)
-	
 	francis_lists_url=self.browser.current_url
 	self.assertRegexpMatches(francis_lists_url,'/lists/.+')
 	self.assertNotEqual(francis_lists_url,edith_list_url)
-	
 	page_text=self.browser.find_element_by_tag_name('body').text
 	self.assertNotIn('Buy peacock feathers',page_text)
 	self.assertIn('Buy milk',page_text)
