bdd9b598163da4b88454335784165b0bfba568af
houyahui <houyahui@t2cloud.net>
2016-08-09 14:57:01 +0800

    migrate db data to database

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,28 +9,41 @@ UPDATE  Make sure import it:  from django.contrib.staticfiles.testing import Sta
 '''
 from django.test import LiveServerTestCase
 import time
+import sys
 import unittest
 class NewVisitorTest(StaticLiveServerTestCase,unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+	for arg in sys.argv:
+	    if 'liveserver' in arg:
+	        cls.server_url='http://'+arg.split('=')[1]
+		return
+	super(StaticLiveServerTestCase,cls).setUpClass()
+        cls.server_url=cls.live_server_url
+    @classmethod
+    def tearDownClass(cls):
+	if cls.server_url==cls.live_server_url:
+	    super(StaticLiveServerTestCase,cls).tearDownClass()
+    def setUp(self):
+        self.browser=webdriver.Chrome()
+        #self.browser.implicitly_wait(15)
+	#self.live_server_url='http://localhost:8000/'
+    def tearDown(self):
+	self.browser.refresh()
+	self.browser.quit()
     def test_layout_and_styling(self):
-	self.browser.get(self.live_server_url)
+	self.browser.get(self.server_url)
 	self.browser.set_window_size(1024,768)
 	inputbox=self.browser.find_element_by_id('id_new_item')
 	inputbox.send_keys('testing\n')
 	inputbox=self.browser.find_element_by_id('id_new_item')
 	self.assertAlmostEqual(inputbox.location['x']+inputbox.size['width']/2,512,delta=10)
-    def setUp(self):
-        self.browser=webdriver.Chrome()
-        self.browser.implicitly_wait(15)
-	#self.live_server_url='http://localhost:8000/'
-    def tearDown(self):
-	self.browser.refresh()
-	self.browser.quit()
     def check_for_row_in_list_table(self,row_text):
 	table=self.browser.find_element_by_id('id_list_table')
 	rows=self.browser.find_elements_by_tag_name('tr')
 	self.assertIn(row_text,[row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
-	self.browser.get(self.live_server_url)
+	self.browser.get(self.server_url)
 	self.assertIn('To-Do',self.browser.title)
 	header_text=self.browser.find_element_by_tag_name('h1').text
 	self.assertIn('To-Do',header_text)
@@ -49,7 +62,7 @@ class NewVisitorTest(StaticLiveServerTestCase,unittest.TestCase):
 	self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 	self.browser.quit()
 	self.browser=webdriver.Chrome()
-	self.browser.get(self.live_server_url)
+	self.browser.get(self.server_url)
 	page_text=self.browser.find_element_by_tag_name('body').text
 	self.assertNotIn('Buy peacock feathers',page_text)
 	self.assertNotIn('make a fly',page_text)
