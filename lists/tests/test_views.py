a6a73708328bd1826b8889f3c090ba3a7498470c
aahyhaa <aahyhaa@126.com>
2016-08-21 11:27:29 +0800

    添加数据库层校验

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -64,33 +64,8 @@ class HomePageTest(TestCase):
 	response=home_page(request)
 	#print repr(response.content)
 	#print response.content
-	self.assertTrue(response.content.startswith('<html>'))
+	self.assertTrue(response.content.startswith('<!DOCTYPE html>'))
 	self.assertIn('<title>To-Do lists</title>',response.content)
 	self.assertTrue(response.content.strip().endswith('</html>'))	
 	expected_html=render_to_string('home.html')
 	self.assertEqual(response.content,expected_html)
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-	list_=List()
-	list_.save()
-	first_item=Item()
-	first_item.text="The first list item"
-	first_item.list=list_
-	first_item.save()
-	second_item=Item()
-	second_item.text='Item the second'
-	second_item.list=list_
-	second_item.save()
-	
-	saved_list=List.objects.first()
-	self.assertEqual(list_,saved_list)
-	saved_items=Item.objects.all()
-	self.assertEqual(saved_items.count(),2)
-	
-	first_saved_item=saved_items[0]
-	second_saved_item=saved_items[1]
-	self.assertEqual(first_saved_item.text,'The first list item')
-	self.assertEqual(list_,first_saved_item.list)
-	self.assertEqual(list_,second_saved_item.list)
-	self.assertEqual(second_saved_item.text,'Item the second')
-	
