66b4d98d6c58c881dd6b42b482ec369851c35025
aahyhaa <aahyhaa@126.com>
2016-08-30 00:22:05 +0800

    因为页面提交的url末尾缺少/,跟urls定义的url不匹配，导致莫名其妙的问题

--- lists/views.py
+++ lists/views.py
@@ -11,19 +11,14 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    import pdb
-    pdb.set_trace()
     error=None
     if request.method == "POST":
         try:
             item=Item(text=request.POST["item_text"],list=list_)
             item.full_clean()
             item.save()
-            print("#######################################")
-            print(item.text)
             return redirect("/lists/%d/" % (list_.id,))
         except ValidationError:
-            print("##################")
             error="You can't have an empty list item"
     return render(request, 'list.html', {'list': list_,'error':error})
 
